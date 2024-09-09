633b7e2aef4f17982bcc1053068bdf5bc8acd12e
RomuloAlves2507 <romuloa.l2507@gmail.com>
2024-09-09 10:07:16 -0300

    Update sensors.cpp
    deixei comentado a implementação do botao reset para caso a gente resolva usar algum momento, alterado o sensor para o novo do Piske

--- esp32_Firmware/sensors.cpp
+++ esp32_Firmware/sensors.cpp
@@ -7,43 +7,58 @@ DHT dht(PIN_DHT, DHTTYPE);
 
 //TODO: COLOCAR LINKS PRAS DOCUMENTACOES E DATASHEETS
 
-//####################### FUNÇÕES DE LEITURA DOS DADOS DO ADS1115
+float reference_battery_value(){
+  // float ref_battery_value = analogRead(PIN_VREF_BATTERY);
+  // return ref_battery_value* 5 / 4095.0;
+  
+  ads_1115.setGain(GAIN_ONE);
+
+  int16_t result = ads_1115.readADC_SingleEnded(3);
+  float voltageRead = ads_1115.computeVolts(result);
+
+  return voltageRead;
+}
+
 float get_motor_current(){
     //como o sinal de tensão q vem do sensor de corrente varia no max 0.625V, 
     //acho que o ganho 4 é melhor pq é mais preciso (mas aí precisa ler só o offset de tensão):
     // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit =      0.125mV
     // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit =       0.03125mV
     ads_1115.setGain(GAIN_ONE);        // 1x gain   + 4.096V  1 bit = 0.0625 //TESTAR OUTROS GANHOS
-    
+   
     int16_t result = ads_1115.readADC_SingleEnded(ADS1115_MOTOR_CURRENT);
     float voltageRead = ads_1115.computeVolts(result);
-    float hallAmps = 0;
 
-    hallAmps = (voltageRead - VREF_100A) * (HSTS016L_NOMINAL_CURRENT_100A / HSTS016L_VARIATION);
+    float motorAmps = (voltageRead / RESISTANCE_MOTOR);
 
-    return hallAmps;
+    return motorAmps;
 }
 
 float get_battery_current(){
-    ads_1115.setGain(GAIN_ONE);        // 1x gain   + 4.096V  1 bit = 0.0625
-
+  ads_1115.setGain(GAIN_ONE);        // 1x gain   + 4.096V  1 bit = 0.0625
+  
+  float VREF_BATTERY = reference_battery_value();
+  
   int16_t result = ads_1115.readADC_SingleEnded(ADS1115_BATTERY_CURRENT);
   float voltageRead = ads_1115.computeVolts(result);
 
   float hallAmps = 0;
-  
-  hallAmps = (voltageRead - VREF_150A) * (HSTS016L_NOMINAL_CURRENT_150A / HSTS016L_VARIATION);
+
+  hallAmps = (voltageRead - VREF_BATTERY) * (200 / HSTS016L_VARIATION);
 
   return hallAmps;
 }
 
 float get_font_voltage(){
-  ads_1115.setGain(GAIN_ONE);        // 1x gain   + 4.096V  1 bit = 0.0625
+  // ads_1115.setGain(GAIN_ONE);        // 1x gain   + 4.096V  1 bit = 0.0625
 
-  int16_t result = ads_1115.readADC_SingleEnded(ADS1115_FONT_VOLTAGE);
-  float voltageRead = result*(2*LIMIT_GAIN_ONE/(RESOLUTION_16BIT-1.0))/1000.0; 
+  // int16_t result = ads_1115.readADC_SingleEnded(ADS1115_FONT_VOLTAGE);
+  // float voltageRead = ads_1115.computeVolts(result); 
   //Find voltage before voltage divider
   float R1 = 47000.0, R2 = 15000.0;  
+
+  float ref_motor_value = analogRead(35);
+  float voltageRead = ref_motor_value* 3.3 / 4095.0;
   
   return voltageRead * (R1 + R2)/R2;
 }
@@ -111,3 +126,18 @@ void setupDHT(){
 void setupADS(){
     ads_1115.begin();
 }
+
+//Implementação do botão Reset
+/*
+void initResetButton() {
+  pinMode(PIN_RESET_BUTTON, INPUT);
+}
+
+void checkResetButton() {
+  if (digitalRead (PIN_RESET_BUTTON) == HIGH) {
+    Serial.println("RESETOU!!!");
+    delay(2000);
+    ESP.restart();
+  }
+}
+*/
